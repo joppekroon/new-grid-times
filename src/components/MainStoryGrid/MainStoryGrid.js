efb4e582f1f950512100b5fa1f5468f5d503dde4
Joppe Kroon <J.Kroon@topdesk.com>
2021-09-16 11:22:34 +0200

    Ex 3: Main grid

--- src/components/MainStoryGrid/MainStoryGrid.js
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -12,7 +12,7 @@ import MainStory from '../MainStory';
 import SecondaryStory from '../SecondaryStory';
 import OpinionStory from '../OpinionStory';
 import Advertisement from '../Advertisement';
-import { COLORS } from '../../constants';
+import { COLORS, QUERIES } from '../../constants';
 
 const MainStoryGrid = () => {
   return (
@@ -58,14 +58,43 @@ const Wrapper = styled.div`
     'advertisement';
   gap: 48px;
   margin-bottom: 48px;
+  
+  @media ${QUERIES.tabletAndUp} {
+    grid-template-columns: 2fr 252px;
+    grid-template-areas:
+      'main-story secondary-stories'
+      'advertisement advertisement'
+      'opinion-stories opinion-stories'
+    ;
+    gap: 48px 16px;
+  }
+  
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: 1fr 402px 273px;
+    grid-template-areas:
+      'main-story secondary-stories opinion-stories'
+      'main-story advertisement advertisement'
+    ;
+    gap: 16px;
+  }
 `;
 
 const MainStorySection = styled.section`
   grid-area: main-story;
+  
+  @media ${QUERIES.tabletAndUp} {
+    border-right: 1px solid ${COLORS.gray[300]};
+    padding-right: 16px;
+  }
 `;
 
 const SecondaryStorySection = styled.section`
   grid-area: secondary-stories;
+  
+  @media ${QUERIES.laptopAndUp} {
+    border-right: 1px solid ${COLORS.gray[300]};
+    padding-right: 16px;
+  }
 `;
 
 const StoryList = styled.div`
@@ -87,6 +116,11 @@ const VerticalStoryWrapper = styled.div`
 
 const AdvertisementSection = styled.section`
   grid-area: advertisement;
+  
+  @media ${QUERIES.laptopAndUp} {
+    border-top: 1px solid ${COLORS.gray[300]};
+    padding-top: 16px;
+  }
 `;
 
 export default MainStoryGrid;
